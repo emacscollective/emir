510f05b79e8d46c7586fcbee146d01a11900c4d0
Jonas Bernoulli <jonas@bernoul.li>
2017-09-19 14:17:00 +0200

    Rearrange emir.el (5/9) Move convenience commands

--- emir.el
+++ emir.el
@@ -488,6 +488,103 @@ This variable should only be used as a last resort."
         (delete-directory (magit-git-dir (concat "modules/" name)) t)))))
 
 ;;;; Convenience
+
+;;;###autoload
+(defun emir-find-file (filename &optional wildcards)
+  (interactive (emir-find-file-read-args "Find file: "))
+  (emir-find-file-noselect filename #'switch-to-buffer wildcards))
+
+;;;###autoload
+(defun emir-find-file-other-window (filename &optional wildcards)
+  (interactive (emir-find-file-read-args "Find file in other window: "))
+  (emir-find-file-noselect filename #'switch-to-buffer-other-window wildcards))
+
+;;;###autoload
+(defun emir-find-file-other-frame (filename &optional wildcards)
+  (interactive (emir-find-file-read-args "Find file in other frame: "))
+  (emir-find-file-noselect filename #'switch-to-buffer-other-frame wildcards))
+
+(defun emir-find-file-read-args (prompt)
+  (with-epkg-repository (epkg (epkg-read-package "Find file of package: "))
+    (find-file-read-args prompt (confirm-nonexistent-file-or-buffer))))
+
+(defun emir-find-file-noselect (filename:s switch &optional wildcards)
+  (let ((value (find-file-noselect filename:s nil nil wildcards)))
+    (if (listp value)
+	(let ((buffers (nreverse value)))
+	  (funcall switch (car buffers))
+	  (mapc #'switch-to-buffer (cdr buffers))
+	  buffers)
+      (funcall switch value))))
+
+;;;###autoload
+(defun emir-find-org-file (name)
+  (interactive
+   (list (completing-read "Find org file: "
+                          '("maint" "config"
+                            "compare" "emacsorphanage" "issues"
+                            "kludges" "melpa-missing" "notes"))))
+  (find-file-other-frame
+   (cond ((equal name "maint")
+          "~/.emacs.d/lib/emir/emir.org")
+         ((equal name "config")
+          (expand-file-name "emir.org" epkg-repository))
+         (t
+          (expand-file-name (concat name ".org") emir-stats-repository)))))
+
+;;;###autoload
+(defun emir-describe-package (package)
+  (interactive
+   (list (epkg-read-package "Describe package: "
+                            (or (tabulated-list-get-id)
+                                (--when-let (symbol-at-point)
+                                  (symbol-name it))))))
+  (help-setup-xref (list #'emir-describe-package package)
+                   (called-interactively-p 'interactive))
+  (with-help-window (help-buffer)
+    (with-current-buffer standard-output
+      (let ((epkg-describe-package-slots-width 14))
+        (epkg-describe-package-1
+         (epkg package)
+         '(type
+           hash
+           url
+           mirror-url
+           mirror-name
+           upstream-user
+           upstream-name
+           upstream-branch
+           upstream-tree
+           library
+           epkg-insert-repopage
+           epkg-insert-homepage
+           epkg-insert-mirrorpage
+           epkg-insert-wikipage
+           license
+           created
+           updated
+           summary
+           libraries
+           provided
+           required
+           epkg-insert-keywords
+           epkg-insert-authors
+           epkg-insert-maintainers
+           epkg-insert-commentary
+           epkg-insert-dependencies
+           epkg-insert-reverse-dependencies))))))
+
+;;;###autoload
+(defun emir-generate-reports ()
+  (interactive)
+  (let ((org-confirm-babel-evaluate nil))
+    (org-publish
+     `("emir"
+       :base-extension      "org"
+       :base-directory      ,emir-stats-repository
+       :publishing-function org-html-publish-to-html)
+     t)))
+
 ;;; Git
 ;;;; Import
 
@@ -831,109 +928,10 @@ This variable should only be used as a last resort."
                          "emacsmirror")
                        (oref pkg mirror-name))))
 
-;;; -
+;;; Urls
 
 (defun emir--lookup-url (url)
   (caar (epkg-sql [:select name :from packages :where (= url $s1)] url)))
-
-;;; -
-
-;;;###autoload
-(defun emir-find-file (filename &optional wildcards)
-  (interactive (emir-find-file-read-args "Find file: "))
-  (emir-find-file-noselect filename #'switch-to-buffer wildcards))
-
-;;;###autoload
-(defun emir-find-file-other-window (filename &optional wildcards)
-  (interactive (emir-find-file-read-args "Find file in other window: "))
-  (emir-find-file-noselect filename #'switch-to-buffer-other-window wildcards))
-
-;;;###autoload
-(defun emir-find-file-other-frame (filename &optional wildcards)
-  (interactive (emir-find-file-read-args "Find file in other frame: "))
-  (emir-find-file-noselect filename #'switch-to-buffer-other-frame wildcards))
-
-(defun emir-find-file-read-args (prompt)
-  (with-epkg-repository (epkg (epkg-read-package "Find file of package: "))
-    (find-file-read-args prompt (confirm-nonexistent-file-or-buffer))))
-
-(defun emir-find-file-noselect (filename:s switch &optional wildcards)
-  (let ((value (find-file-noselect filename:s nil nil wildcards)))
-    (if (listp value)
-	(let ((buffers (nreverse value)))
-	  (funcall switch (car buffers))
-	  (mapc #'switch-to-buffer (cdr buffers))
-	  buffers)
-      (funcall switch value))))
-
-;;;###autoload
-(defun emir-find-org-file (name)
-  (interactive
-   (list (completing-read "Find org file: "
-                          '("maint" "config"
-                            "compare" "emacsorphanage" "issues"
-                            "kludges" "melpa-missing" "notes"))))
-  (find-file-other-frame
-   (cond ((equal name "maint")
-          "~/.emacs.d/lib/emir/emir.org")
-         ((equal name "config")
-          (expand-file-name "emir.org" epkg-repository))
-         (t
-          (expand-file-name (concat name ".org") emir-stats-repository)))))
-
-;;; Urls
-
-;;;###autoload
-(defun emir-describe-package (package)
-  (interactive
-   (list (epkg-read-package "Describe package: "
-                            (or (tabulated-list-get-id)
-                                (--when-let (symbol-at-point)
-                                  (symbol-name it))))))
-  (help-setup-xref (list #'emir-describe-package package)
-                   (called-interactively-p 'interactive))
-  (with-help-window (help-buffer)
-    (with-current-buffer standard-output
-      (let ((epkg-describe-package-slots-width 14))
-        (epkg-describe-package-1
-         (epkg package)
-         '(type
-           hash
-           url
-           mirror-url
-           mirror-name
-           upstream-user
-           upstream-name
-           upstream-branch
-           upstream-tree
-           library
-           epkg-insert-repopage
-           epkg-insert-homepage
-           epkg-insert-mirrorpage
-           epkg-insert-wikipage
-           license
-           created
-           updated
-           summary
-           libraries
-           provided
-           required
-           epkg-insert-keywords
-           epkg-insert-authors
-           epkg-insert-maintainers
-           epkg-insert-commentary
-           epkg-insert-dependencies
-           epkg-insert-reverse-dependencies))))))
-
-(defun emir-generate-reports ()
-  (interactive)
-  (let ((org-confirm-babel-evaluate nil))
-    (org-publish
-     `("emir"
-       :base-extension      "org"
-       :base-directory      ,emir-stats-repository
-       :publishing-function org-html-publish-to-html)
-     t)))
 
 (defvar emir-url-history nil)
 
