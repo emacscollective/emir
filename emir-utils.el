7996d0040823fd40615c610e153e863011c1b795
Jonas Bernoulli <jonas@bernoul.li>
2022-01-01 11:30:14 +0100

    Bump copyright years

--- emir-utils.el
+++ emir-utils.el
@@ -1,6 +1,6 @@
 ;;; emir-utils.el --- non-essential utilities     -*- lexical-binding: t -*-
 
-;; Copyright (C) 2016-2021  Jonas Bernoulli
+;; Copyright (C) 2016-2022  Jonas Bernoulli
 
 ;; Author: Jonas Bernoulli <jonas@bernoul.li>
 ;; Homepage: https://github.com/emacscollective/emir
