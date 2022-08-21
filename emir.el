e3916d0d6c6c6a25aa966177a1f4002d2bc5cfa8
Jonas Bernoulli <jonas@bernoul.li>
2022-08-21 21:30:04 +0200

    Pass type vector to epkgs

--- emir.el
+++ emir.el
@@ -347,7 +347,7 @@ Mirror as an `epkg-core-package' instead? " name))))))
 (defun emir-update-wiki-packages (&optional from recreate)
   (interactive (list (and current-prefix-arg
                           (epkg-read-package "Limit to packages after: "))))
-  (dolist (pkg (epkgs nil #'epkg-wiki-package-p))
+  (dolist (pkg (epkgs nil [wiki]))
     (let ((name (oref pkg name)))
       (when (or (not from) (string< from name))
         (if (assoc name emir-suspended-packages)
@@ -363,18 +363,16 @@ Mirror as an `epkg-core-package' instead? " name))))))
 (defun emir-update-other-packages (&optional from recreate)
   (interactive (list (and current-prefix-arg
                           (epkg-read-package "Limit to packages after: "))))
-  (dolist (pkg (epkgs nil #'epkg-mirrored-package--eieio-childp))
-    (unless (or (cl-typep pkg 'epkg-github-package)
-                (cl-typep pkg 'epkg-wiki-package))
-      (let ((name (oref pkg name)))
-        (when (or (not from) (string< from name))
-          (if (assoc name emir-suspended-packages)
-              (message "Skipping suspended %s" name)
-            (message "Updating %s..." name)
-            (if recreate
-                (emir-update (epkg name) t)
-              (emir-update-package name))
-            (message "Updating %s...done" name))))))
+  (dolist (pkg (epkgs nil [mirrored* !github* !wiki]))
+    (let ((name (oref pkg name)))
+      (when (or (not from) (string< from name))
+        (if (assoc name emir-suspended-packages)
+            (message "Skipping suspended %s" name)
+          (message "Updating %s..." name)
+          (if recreate
+              (emir-update (epkg name) t)
+            (emir-update-package name))
+          (message "Updating %s...done" name)))))
   (emir-commit (emir--update-message) nil :dump))
 
 ;;;###autoload
@@ -421,7 +419,8 @@ Mirror as an `epkg-core-package' instead? " name))))))
 
 ;;;###autoload
 (defun emir-update-branch (name)
-  (interactive (list (epkg-read-package "Update branch of: ")))
+  (interactive (list (epkg-read-package "Update branch of: " nil
+                                        epkg--trivial-package-predicates)))
   (let* ((pkg (epkg name))
          (url (oref pkg url))
          (value (oref pkg upstream-branch)))
@@ -619,8 +618,7 @@ Mirror as an `epkg-core-package' instead? " name))))))
   (setq emir-failed nil)
   (setq message-log-max 20000)
   (mapc #'emir-setup-module
-        (epkgs 'name '(epkg-mirrored-package--eieio-childp
-                       epkg-shelved-package--eieio-childp))))
+        (epkgs 'name [mirrored* shelved])))
 
 ;;;; Migrate
 
@@ -1290,7 +1288,7 @@ Mirror as an `epkg-core-package' instead? " name))))))
                        ,@(if (functionp query)
                              (funcall query pkg)
                            query)))
-                   (epkgs nil #'epkg-github-package--eieio-childp))
+                   (epkgs nil [github*]))
            (or per-page 100)))
          (length (length groups)))
     (cl-labels
