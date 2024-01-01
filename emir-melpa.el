191dfcc4356fb38fc668eef297f9c8decdf0e768
Jonas Bernoulli <jonas@bernoul.li>
2024-01-01 23:14:53 +0100

    Bump copyright years

--- emir-melpa.el
+++ emir-melpa.el
@@ -1,6 +1,6 @@
 ;;; emir-melpa.el --- Import Melpa recipes  -*- lexical-binding:t -*-
 
-;; Copyright (C) 2016-2023 Jonas Bernoulli
+;; Copyright (C) 2016-2024 Jonas Bernoulli
 
 ;; Author: Jonas Bernoulli <jonas@bernoul.li>
 ;; Homepage: https://github.com/emacscollective/emir
