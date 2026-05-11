648ab01e516f52d839f067132af28e6800d96148
Jonas Bernoulli <jonas@bernoul.li>
2026-05-11 16:03:47 +0200

    Update cond-let shorthands

--- emir-utils.el
+++ emir-utils.el
@@ -163,11 +163,16 @@
 (provide 'emir-utils)
 ;; Local Variables:
 ;; read-symbol-shorthands: (
-;;   ("and$"      . "cond-let--and$")
-;;   ("and-let"   . "cond-let--and-let")
-;;   ("if-let"    . "cond-let--if-let")
-;;   ("when$"     . "cond-let--when$")
-;;   ("when-let"  . "cond-let--when-let")
-;;   ("while-let" . "cond-let--while-let"))
+;;   ("and$"       . "cond-let--and$")
+;;   ("thread$"    . "cond-let--thread$")
+;;   ("when$"      . "cond-let--when$")
+;;   ("and-let*"   . "cond-let--and-let*")
+;;   ("and-let"    . "cond-let--and-let")
+;;   ("if-let*"    . "cond-let--if-let*")
+;;   ("if-let"     . "cond-let--if-let")
+;;   ("when-let*"  . "cond-let--when-let*")
+;;   ("when-let"   . "cond-let--when-let")
+;;   ("while-let*" . "cond-let--while-let*")
+;;   ("while-let"  . "cond-let--while-let"))
 ;; End:
 ;;; emir-utils.el ends here
